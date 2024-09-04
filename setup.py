4d7fd0a7d7e9fcb759f9d2be9a51f63ec0242f72
rafaelgpaves <rafaelgp3@al.insper.edu.br>
2024-09-04 14:30:38 -0300

    chore: setup file

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dev_aberto_rafaelgp3',
+      version='0.1',
+      packages=['dev_aberto'],
+      author='Rafael G. P',
+      python_requires='>=3.7'
+      )
